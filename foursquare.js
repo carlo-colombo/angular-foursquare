d223567fb46804ea8400e42a8976718ff5c420d1
Carlo Colombo <carlo.colombo@openmindonline.it>
2013-06-09 08:29:06 +0200

    4 spaces ->  2 spaces

--- foursquare.js
+++ foursquare.js
@@ -1,98 +1,98 @@
 angular.module('ngFoursquare',["ngResource"])
-    .constant('BASE_API_URL','https://api.foursquare.com/v2')
-    .constant('encodeParam',function (data) {
-        return data && Object.keys(data).map(function (k) {
-            return encodeURI(k)+'='+encodeURI(data[k])
-        }).join('&')  
-    })
-    .config(function ($provide,$resourceProvider,$httpProvider) {
-        var $hp = angular.copy($httpProvider)
-            ,$rp = angular.copy($resourceProvider)
-            i = 0
+  .constant('BASE_API_URL','https://api.foursquare.com/v2')
+  .constant('encodeParam',function (data) {
+    return data && Object.keys(data).map(function (k) {
+      return encodeURI(k)+'='+encodeURI(data[k])
+    }).join('&')  
+  })
+  .config(function ($provide,$resourceProvider,$httpProvider) {
+    var $hp = angular.copy($httpProvider)
+      ,$rp = angular.copy($resourceProvider)
+      i = 0
 
-        $provide.decorator('$cacheFactory',function ($delegate) {
-            return function (cacheId,options) {
-                if(cacheId=='$http'){
-                    cacheId+=''+i++
-                }
-                $delegate(cacheId,options)
-            }
+    $provide.decorator('$cacheFactory',function ($delegate) {
+      return function (cacheId,options) {
+        if(cacheId=='$http'){
+          cacheId+=''+i++
+        }
+        $delegate(cacheId,options)
+      }
+    })
+    
+    delete $hp.defaults.headers.common["X-Requested-With"]
+    $hp.defaults.sticazzi=true
+
+    $provide.provider('$customHttp',$hp)
+    $rp.$get[0]="$customHttp"
+    $provide.provider('$customResource',$rp)
+  })
+  .provider('Foursquare',function (encodeParam) {
+    var FoursquareProvider = {
+    '$get': function ($customResource,$q,BASE_API_URL) {
+      var $resource=$customResource
+        ,params = {
+          oauth_token: FoursquareProvider.token || ''
+          ,v: '20130425'
+        }
+        ,Foursquare = {
+        token: function (token) {
+          FoursquareProvider.token = token
+        }
+        ,Users: $resource(BASE_API_URL+'/users/:userId/:action',
+          {},{
+          lists:{
+            method:'GET',
+            params: angular.extend({action:'lists'},params)
+          },
+          get:{
+            method:'GET',
+            params: params
+          }
         })
-        
-        delete $hp.defaults.headers.common["X-Requested-With"]
-        $hp.defaults.sticazzi=true
-
-        $provide.provider('$customHttp',$hp)
-        $rp.$get[0]="$customHttp"
-        $provide.provider('$customResource',$rp)
-    })
-    .provider('Foursquare',function (encodeParam) {
-      var FoursquareProvider = {
-        '$get': function ($customResource,$q,BASE_API_URL) {
-            var $resource=$customResource
-                ,params = {
-                    oauth_token: FoursquareProvider.token || ''
-                    ,v: '20130425'
-                }
-                ,Foursquare = {
-                token: function (token) {
-                  FoursquareProvider.token = token
-                }
-                ,Users: $resource(BASE_API_URL+'/users/:userId/:action',
-                    {},{
-                    lists:{
-                        method:'GET',
-                        params: angular.extend({action:'lists'},params)
-                    },
-                    get:{
-                        method:'GET',
-                        params: params
-                    }
-                })
-                ,Venues  : $resource(BASE_API_URL+'/venues/:venueId/:action',
-                    {},{
-                    search: {
-                        method: 'GET'
-                        ,params: angular.extend({action:'search'},params)
-                    },
-                    get: {
-                        method: 'GET',
-                        params: params
-                    }
-                })
-                ,Checkins: $resource(BASE_API_URL+'/checkins/:checkinId/:action',
-                    {},{
-                    add: {
-                        method: 'POST',
-                        params: angular.extend({action:'add'},params),
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
-                        },
-                        transformRequest: encodeParam
-                    },
-                    get: {
-                        method: 'GET',
-                        params: params
-                    }
-                })
-                ,search:function (position) {
-                    return $q.when(position).then(function (pos) {
-                            var c = pos.coords,
-                                ll = "" + c.latitude + "," + c.longitude
-                            return ll
-                        })
-                        .then(function (ll) {
-                            var deferred = $q.defer()
-                            Foursquare.Venues.search({
-                                ll:ll
-                            },deferred.resolve,deferred.reject)
-                            return deferred.promise
-                        })
-                }
-            }
-            return Foursquare
+        ,Venues  : $resource(BASE_API_URL+'/venues/:venueId/:action',
+          {},{
+          search: {
+            method: 'GET'
+            ,params: angular.extend({action:'search'},params)
+          },
+          get: {
+            method: 'GET',
+            params: params
+          }
+        })
+        ,Checkins: $resource(BASE_API_URL+'/checkins/:checkinId/:action',
+          {},{
+          add: {
+            method: 'POST',
+            params: angular.extend({action:'add'},params),
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            transformRequest: encodeParam
+          },
+          get: {
+            method: 'GET',
+            params: params
+          }
+        })
+        ,search:function (position) {
+          return $q.when(position).then(function (pos) {
+              var c = pos.coords,
+                ll = "" + c.latitude + "," + c.longitude
+              return ll
+            })
+            .then(function (ll) {
+              var deferred = $q.defer()
+              Foursquare.Venues.search({
+                ll:ll
+              },deferred.resolve,deferred.reject)
+              return deferred.promise
+            })
         }
       }
-      return FoursquareProvider
-    })
+      return Foursquare
+    }
+    }
+    return FoursquareProvider
+  })
 
